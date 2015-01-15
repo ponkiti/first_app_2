e838e7696e404a80cadcd3593e7a2869ff37b65d
ponkiti <crivelli01@gmail.com>
2015-01-15 16:50:02 +0900

    Chapter 9.3.3 has finished

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.1.8'
 gem 'bootstrap-sass'
 gem 'sprockets'
 gem 'bcrypt-ruby'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 group :development, :test do
   gem 'rspec-rails'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
 
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    #@users = User.all
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+	if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+	  redirect_to @user
+	else
+	  render 'edit'
+	end
   end
 
   def create
@@ -24,6 +46,18 @@ class UsersController < ApplicationController
     def user_params
 	  params.require(:user).permit(:name, :email, :password,
 	  				:password_confirmation)
+	end
+
+	def signed_in_user
+	  unless signed_in?
+        store_location
+		redirect_to signin_url, notice: "Please sign in."
+	  end
+	end
+
+    def correct_user
+      @user = User.find(params[:id])
+	  redirect_to(root_path) unless current_user?(@user)
 	end
 
 end
